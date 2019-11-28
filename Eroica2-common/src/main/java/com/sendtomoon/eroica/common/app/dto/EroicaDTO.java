b2ea1ea454812a2b9a57b8afdabb99c2b0d8f212
galaxysha512 <galaxysha512@gmail.com>
2019-11-28 08:17:46 +0800

    commit

---
+++ Eroica2-common/src/main/java/com/sendtomoon/eroica/common/app/dto/EroicaDTO.java
@@ -0,0 +1,7 @@
+package com.sendtomoon.eroica.common.app.dto;
+
+import java.io.Serializable;
+
+public class EroicaDTO implements Serializable {
+	static final long serialVersionUID = -4623703295938223949L;
+}
