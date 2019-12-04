0c6d7f55a6a3eaefc96ddbcdf7119248b94a3c31
galaxysha512 <galaxysha512@gmail.com>
2019-12-04 09:39:41 +0800

    commit

---
+++ Eroica2-common/src/main/java/com/sendtomoon/eroica/common/Eroica.java
@@ -0,0 +1,17 @@
+package com.sendtomoon.eroica.common;
+
+/**
+ * Eroica项目部分静态字段
+ *
+ */
+public abstract class Eroica {
+
+	public static final String PARAM_NAME_DATA_TYPE = "eroica_data_type";
+
+	public static final String PARAM_NAME_METHOD_RESULT = "eroica_method_result";
+
+	public static final String DATA_TYPE_MAP = "map";
+
+	public static final String LOG_HOME_KEY = "eroica.log.home";
+
+}
