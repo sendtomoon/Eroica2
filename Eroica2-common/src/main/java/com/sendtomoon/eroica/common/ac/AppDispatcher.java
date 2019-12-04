e3352a1c1312d05fff1a1c1ae4a6bc6fbd0983cc
galaxysha512 <galaxysha512@gmail.com>
2019-12-04 09:40:31 +0800

    commit

---
+++ Eroica2-common/src/main/java/com/sendtomoon/eroica/common/ac/AppDispatcher.java
@@ -0,0 +1,10 @@
+package com.sendtomoon.eroica.common.ac;
+
+import org.springframework.context.ApplicationContextAware;
+
+import com.sendtomoon.eroica.common.app.biz.ac.ApplicationController;
+
+public interface AppDispatcher extends ApplicationController,ApplicationContextAware{
+	
+	 
+}
