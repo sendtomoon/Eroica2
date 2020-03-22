f05ffe1323dfc61fcced9d310e70538808d93820
galaxysha512 <galaxysha512@gmail.com>
2020-03-22 16:47:24 +0800

    commit

---
+++ Eroica2-EroApp/src/main/java/com/sendtomoon/eroica/eoapp/sar/event/SARStartupFailedEvent.java
@@ -0,0 +1,20 @@
+package com.sendtomoon.eroica.eoapp.sar.event;
+
+import com.sendtomoon.eroica.eoapp.sar.SARContext;
+
+public class SARStartupFailedEvent extends SAREvent{
+
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SARStartupFailedEvent(SARContext sarContext){
+		super(sarContext);
+	}
+
+	
+	
+}
---
+++ Eroica2-EroApp/src/main/java/com/sendtomoon/eroica/eoapp/sar/event/SARStartupedEvent.java
@@ -0,0 +1,15 @@
+package com.sendtomoon.eroica.eoapp.sar.event;
+
+import com.sendtomoon.eroica.eoapp.sar.SARContext;
+
+public class SARStartupedEvent extends SAREvent{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SARStartupedEvent(SARContext sarContext){
+		super(sarContext);
+	}
+}
