97145c8177a873da6318f4818619ed243f7ff6b3
galaxysha512 <galaxysha512@gmail.com>
2020-08-17 15:52:45 +0800

    commit

---
+++ Eroica2-common/src/main/java/com/sendtomoon/eroica/common/utils/NamedThreadFactory.java
@@ -0,0 +1,45 @@
+
+package com.sendtomoon.eroica.common.utils;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class NamedThreadFactory implements ThreadFactory
+{
+	
+	private final AtomicInteger threadNum = new AtomicInteger(1);
+
+	private final String prefix;
+
+	private final boolean daemo;
+
+	private final ThreadGroup group;
+
+
+	public NamedThreadFactory(String prefix)
+	{
+		this(prefix,false);
+	}
+
+	public NamedThreadFactory(String prefix,boolean daemo)
+	{
+		this.prefix = prefix;
+		this.daemo = daemo;
+        SecurityManager s = System.getSecurityManager();
+        this. group = ( s == null ) ? Thread.currentThread().getThreadGroup() : s.getThreadGroup();
+	}
+
+	public Thread newThread(Runnable runnable)
+	{
+		String name = prefix + threadNum.getAndIncrement();
+        Thread ret = new Thread(group,runnable,name,0);
+        ret.setDaemon(daemo);
+        return ret;
+	}
+
+	public ThreadGroup getThreadGroup()
+	{
+		return group;
+	}
+}
